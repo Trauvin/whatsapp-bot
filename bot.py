06764d6fe054f38a7825960e9e67e4b92ebbad36
Trauvin <mateusgalote@gmail.com>
2021-07-13 15:41:07 -0300

    init

---
+++ bot.py
@@ -0,0 +1,45 @@
+from flask import Flask, request
+import requests
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    incoming_msg = request.values.get('Body', '').lower() # pega a message enviada pelo usuário
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+
+    if 'nun' in incoming_msg:
+        # retorne uma citação 
+        msg.body("A linha da pobreza no Brasil.")
+        responded = True
+    
+    if 'brasil' and 'descoberto' in incoming_msg:
+        msg.body("No dia 26 de Janeiro de 1500, por Pedro Álvares Cabral.")
+        responded = True
+
+    if 'gato' in incoming_msg or 'gata' in incoming_msg:
+        # retorne uma foto de gato
+        msg.media('https://cataas.com/cat')
+        responded = True
+    
+    if 'citação' in incoming_msg:
+        # retorne uma citação 
+        r = requests.get('https://api.quotable.io/random')
+        if r.status_code == 200:
+            data = r.json()
+            quote = f'{data["content"]} ({data["author"]})'
+        else:
+            quote = 'Não consegui recuperar uma citação neste momento, desculpe.'
+        msg.body(quote)
+        responded = True
+
+    if not responded:
+        msg.body('Só conheço frases e gatos famosos, desculpe!')
+    return str(resp)
+
+if __name__ == '__main__':
+   app.run()
